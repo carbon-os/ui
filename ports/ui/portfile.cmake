a7f2e08d37eed470831f6b3b4d7f4c812068371e
carbon-os <259760037+carbon-os@users.noreply.github.com>
2026-04-04 10:03:14 -0700

    updates

--- ports/ui/portfile.cmake
+++ ports/ui/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL             https://github.com/carbon-os/ui
-    REF             bcb6e142686753b9128d86dd2afaaaaddc875073
+    REF             441599f0b5c17c3dcd412a395019ffc851506d4a
     HEAD_REF        main
 )
 
