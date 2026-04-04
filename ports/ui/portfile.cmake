a50d29dfd5a0fbc5a2ca1dd564f6410e3c26f18d
carbon-os <259760037+carbon-os@users.noreply.github.com>
2026-04-04 09:51:45 -0700

    updates

--- ports/ui/portfile.cmake
+++ ports/ui/portfile.cmake
@@ -1,10 +1,7 @@
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL             https://github.com/carbon-os/ui
-    HEAD_REF        main
-    # Once you tag releases, replace HEAD_REF with:
-    # REF             <full-commit-sha>
-    # HEAD_REF        main
+    REF             bcb6e142686753b9128d86dd2afaaaaddc875073
 )
 
 vcpkg_cmake_configure(
@@ -19,8 +16,6 @@ vcpkg_cmake_config_fixup(
     CONFIG_PATH lib/cmake/ui
 )
 
-# Remove duplicate headers from the debug tree
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
-# vcpkg requires a copyright/license file
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
